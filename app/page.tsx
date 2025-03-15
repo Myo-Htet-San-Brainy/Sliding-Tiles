7a269eebbff67c2e50efdccaa841343faff9e4cb
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2025-03-15 07:28:38 +0700

    dividing img into squares and draw them on canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,120 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef } from "react";
+
+const page = () => {
+  const sourceImgRef = useRef<null | HTMLImageElement>(null);
+  const destCanvasRef = useRef<null | HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (destCanvasRef.current && sourceImgRef.current) {
+      const ctx = destCanvasRef.current.getContext("2d");
+      //set canvas size same as pic size
+      console.log("natural w", sourceImgRef.current?.naturalWidth);
+      console.log("natural h", sourceImgRef.current?.naturalHeight);
+
+      console.log(" w", sourceImgRef.current?.width);
+      console.log(" h", sourceImgRef.current?.height);
+
+      destCanvasRef.current.width = sourceImgRef.current?.naturalWidth;
+      destCanvasRef.current.height = sourceImgRef.current?.naturalHeight;
+
+      //no of rows, no of cols
+      const pieceSize = 100;
+      const rows = 500 / pieceSize;
+      const cols = 500 / pieceSize;
+      //get data ready - sx, sy, sw, sh, dx, dy, dw, dh
+      const pieces = [];
+      for (let r = 0; r < Array(rows).length; r++) {
+        for (let y = 0; y < Array(cols).length; y++) {
+          const sx = r * pieceSize;
+          const sy = y * pieceSize;
+          const sw = pieceSize;
+          const sh = pieceSize;
+          const dx = r * pieceSize;
+          const dy = y * pieceSize;
+          const dw = pieceSize;
+          const dh = pieceSize;
+          pieces.push({ sx, sy, sw, sh, dx, dy, dw, dh });
+        }
+      }
+      // render
+      pieces.forEach((piece, index) => {
+        // console.log(`piece ${index}:`, {
+        //   sx: piece.sx,
+        //   sy: piece.sy,
+        //   sw: piece.sw,
+        //   sh: piece.sh,
+        //   dx: piece.dx,
+        //   dy: piece.dy,
+        //   dw: piece.dw,
+        //   dh: piece.dh,
+        // });
+        ctx?.drawImage(
+          sourceImgRef.current as HTMLImageElement,
+          piece.sx,
+          piece.sy,
+          piece.sw,
+          piece.sh,
+          piece.dx,
+          piece.dy,
+          piece.dw,
+          piece.dh
+        );
+      });
+
+      //   sx: pieces[0].sx,
+      //   sy: pieces[0].sy,
+      //   sw: pieces[0].sw,
+      //   sh: pieces[0].sh,
+      //   dx: pieces[0].dx,
+      //   dy: pieces[0].dy,
+      //   dw: pieces[0].dw,
+      //   dh: pieces[0].dh,
+      // });
+
+      // ctx?.drawImage(
+      //   sourceImgRef.current as HTMLImageElement,
+      //   pieces[0].sx,
+      //   pieces[0].sy,
+      //   pieces[0].sw,
+      //   pieces[0].sh,
+      //   pieces[0].dx,
+      //   pieces[0].dy,
+      //   pieces[0].dw,
+      //   pieces[0].dh
+      // );
+
+      // console.log(`piece 1:`, {
+      //   sx: pieces[1].sx,
+      //   sy: pieces[1].sy,
+      //   sw: pieces[1].sw,
+      //   sh: pieces[1].sh,
+      //   dx: pieces[1].dx,
+      //   dy: pieces[1].dy,
+      //   dw: pieces[1].dw,
+      //   dh: pieces[1].dh,
+      // });
+
+      // ctx?.drawImage(
+      //   sourceImgRef.current as HTMLImageElement,
+      //   pieces[1].sx,
+      //   pieces[1].sy,
+      //   pieces[1].sw,
+      //   pieces[1].sh,
+      //   pieces[1].dx,
+      //   pieces[1].dy,
+      //   pieces[1].dw,
+      //   pieces[1].dh
+      // );
+    }
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <canvas ref={destCanvasRef}></canvas>
+      <img ref={sourceImgRef} src="/goodboi.jpg" alt="idk" className="hidden" />
     </div>
   );
-}
+};
+export default page;
